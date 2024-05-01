b08b25c4a7d67d163a1d39ab65ec8139cc4c9980
codeomech <harshit.mangla@capgemini.com>
2024-05-01 23:50:38 +0530

    hooks-useState-Top-rated

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,12 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
+import {
+    useState
+} from "react";
 import { restaurants } from "../constants/mockResList";
 
-const CardContainer = () => (
-    <div className="cardContainer">
-        {restaurants.map((restaurant) => (
-            <RestaurantCard key={restaurant.info?.id} resData={restaurant} />
-        ))}
-    </div>
-)
+const CardContainer = () => {
+    const [listofRestaurant, setListofRestaurant] = useState(restaurants)
+    return (
+        <div className="body">
+            <button className="filter-btn" onClick={() => {
+                const filteredList = listofRestaurant.filter((res) => res.info.avgRating > 4.3)
+                setListofRestaurant(filteredList);
+            }}>Top Rated</button>
+            <div className="cardContainer">
+                {listofRestaurant.map((restaurant) => (
+                    <RestaurantCard key={restaurant.info?.id} resData={restaurant} />
+                ))}
+            </div>
+        </div>
+    )
+}
 
 export default CardContainer;
