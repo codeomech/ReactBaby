79ef3302ecff11cf36a0f1d4087d85f368180a9f
codeomech <harshit.mangla@capgemini.com>
2024-05-03 02:08:59 +0530

    hooks-useEffect-shimmer-UI

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,12 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import {
+    useEffect,
     useState
 } from "react";
-import { restaurants } from "../constants/mockResList";
+import ShimmerComponent from "./Shimmer";
 
 const CardContainer = () => {
-    const [listofRestaurant, setListofRestaurant] = useState(restaurants)
-    return (
+    const [listofRestaurant, setListofRestaurant] = useState([])
+
+    useEffect(() => {
+        console.log("using use effect rendered last");
+        fetchRestData();
+    }, [])
+
+    const fetchRestData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.6342535&lng=73.81139550000002&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+
+        const json = await data.json();
+        setListofRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+        )
+    }
+
+
+    return listofRestaurant.length === 0 ? <ShimmerComponent /> : (
         <div className="body">
             <button className="filter-btn" onClick={() => {
                 const filteredList = listofRestaurant.filter((res) => res.info.avgRating > 4.3)
---
+++ src/components/Shimmer.js
@@ -0,0 +1,20 @@
+const ShimmerComponent = () => {
+    return (
+        <div className="cardContainer">
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+        </div>
+    )
+}
+
+export default ShimmerComponent;
