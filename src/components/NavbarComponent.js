1042a058c029f8fbc35f837fe2cb019bb7e7a525
codeomech <harshit.mangla@capgemini.com>
2024-05-04 18:51:44 +0530

    serch-clear

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,15 +1,24 @@
 
+import { useState } from 'react';
 import logo from '../image/klipartz.com.png';
 
-const NavbarComponent = () => (
-    <div className="navbar">
-        <img className="logo" src={logo} alt="Logo"></img>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
-    </div >
-)
+
+const NavbarComponent = () => {
+    const [btnname, setbtnname] = useState('Login');
+
+    return (
+        <div className="navbar">
+            <img className="logo" src={logo} alt="Logo"></img>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#contact">Contact</a>
+            <a onClick={() => {
+                btnname === 'Login' ? setbtnname('Logout') : setbtnname('Login');
+            }}>{btnname}</a>
+        </div >
+    )
+}
 
 export const varex = 5;
 export default NavbarComponent;
